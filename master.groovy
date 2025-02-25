2e492417bfa72e2394593addad1e6b3b75e843fb
prashanth881 <prashanthvishnu.askan@gmail.com>
2025-02-25 19:26:48 +0530

    Update master.groovy

--- master.groovy
+++ master.groovy
@@ -11,6 +11,11 @@ pipeline {
         stage('Deploy') {
             steps {
                 sh 'npm run deploy --env=prod'
+                // Ensure the destination directory exists
+                    sh 'if [ ! -d /var/www/html/prod ]; then mkdir -p /var/www/html/prod; fi'
+                    
+                    // Use rsync to copy only changed files to the destination directory
+                    sh 'rsync -av --delete new/ /var/www/html/prod/
             }
         }
     }
