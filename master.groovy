04fd2eeb551425499d4a744cd1bb268f18a4bd70
prashanth881 <prashanthvishnu.askan@gmail.com>
2025-02-25 19:49:41 +0700

    added groovy file

---
+++ master.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'npm install'
+                sh 'npm run build'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'npm run deploy --env=prod'
+            }
+        }
+    }
+}
